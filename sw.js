4599c3178db9a1944dfcf58c0aaeaca23a65f910
Lo <contact.boosted.ar@gmail.com>
2026-09-01 20:52:13 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,6 @@
+var C='coach-v2';var F=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',function(e){self.skipWaiting();e.waitUntil(caches.open(C).then(function(c){return c.addAll(F)}).catch(function(){}))});
+self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(k){return Promise.all(k.filter(function(x){return x!==C}).map(function(x){return caches.delete(x)}))}).then(function(){return self.clients.claim()}))});
+self.addEventListener('fetch',function(e){if(e.request.method!=='GET')return;
+ e.respondWith(fetch(e.request).then(function(res){var cp=res.clone();caches.open(C).then(function(c){c.put(e.request,cp)});return res})
+ .catch(function(){return caches.match(e.request).then(function(m){return m||caches.match('./index.html')})}))});
